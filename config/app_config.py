829a81671117d852ad8a11bb326877f4f6f8d810
Krukrukruzhka <misha230503@mail.ru>
2024-12-15 03:08:41 +0300

    refactor user dir

--- config/app_config.py
+++ config/app_config.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from src.datamodels.configs import Config
 from src.utils.constants import REMOTE_ENV, LOCAL_ENV
@@ -22,8 +23,8 @@ def init_app_config(env_mode: str, app_config: Config):
     elif env_mode == LOCAL_ENV:
         app_config.max_requests = 100
         app_config.workers = 1
-        app_config.log_path = "/Users/krukrukruzhka/linal_logs/"
-        app_config.template_path = "/Users/krukrukruzhka/github_projects/LinalEducation/templates"
+        app_config.log_path = os.path.expanduser("~/linal_logs/")
+        app_config.template_path = os.path.expanduser("~/github_projects/LinalEducation/templates")
     else:
         raise RuntimeError(f"Unexpected APPLICATION_ENV value {env_mode}. Use {REMOTE_ENV} or {LOCAL_ENV}.")
 
